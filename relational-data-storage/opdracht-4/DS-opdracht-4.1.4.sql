25d771230877872664ff67a24f68064a1f24b8ac
Annabelmees <annabel01.meester@gmail.com>
2022-10-11 16:34:16 +0200

    DS Opdracht 4.1.4 (#5)

---
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.4.sql
@@ -0,0 +1,6 @@
+SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode FROM mhl_suppliers 
+INNER JOIN mhl_yn_properties ON mhl_suppliers.id = mhl_yn_properties.supplier_ID
+	WHERE propertytype_ID IN (
+        SELECT id FROM mhl_propertytypes WHERE name = 'ook voor particulieren')
+    OR propertytype_ID IN (
+	    SELECT id FROM mhl_propertytypes WHERE name = 'specialistische leverancier');
